edcca6cb41d25b443e1bf3e70aa59a857feafc9d
Jack Davis <jackd942@gmail.com>
2020-04-22 16:50:31 -0500

    tests for new_random

--- lib/brick.ex
+++ lib/brick.ex
@@ -6,22 +6,29 @@ defmodule Tetris.Brick do
     reflection: false 
   ]
 
-  def new(), do: __struct__
+  def new(), do: __struct__()
 
   def new_random do
     %{
       name: random_name(),
       location: {40, 0},
-      rotation: 0,
-      reflection: false 
+      rotation: random_rotation(),
+      reflection: random_reflection() 
     }
   end
 
   def random_name() do
     ~w(i l z o t)a
-    |> Enum.shuffle
-    |> List.first
+    |> Enum.random
   end
 
-  
+  def random_rotation() do
+    [0, 90, 180, 270]
+    |> Enum.random
+  end
+
+  def random_reflection() do
+    [true, false]
+    |> Enum.random
+  end
 end
